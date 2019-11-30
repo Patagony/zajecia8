fa91dacd3975338d1a8e26c6f53a4863db074420
Jacek Kołodziejczyk <patagony07@gmail.com>
2019-11-30 14:47:04 +0100

    dodano wizytowke

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        wyswietl();
 
         double a = 0;
         double b = 1;
@@ -11,4 +15,22 @@ public class Main {
         System.out.println("NAPIS");
         System.out.println("ZMIENNE");
     }
+
+        static void wyswietl() {
+
+            String nazwisko;
+            String imie;
+            int wzrost;
+            String address;
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nazwisko: ");
+            nazwisko = scanner.next();
+            System.out.print("Imie: ");
+            imie = scanner.next();
+            System.out.print("Wzrost: ");
+            wzrost = scanner.nextInt();
+            System.out.print("Adres: ");
+            address = scanner.next();
+        }
 }
