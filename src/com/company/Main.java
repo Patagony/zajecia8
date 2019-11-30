bb0733160880bcf753a35ee933c4132511acf108
Jacek Kołodziejczyk <patagony07@gmail.com>
2019-11-30 13:14:10 +0100

    zmienne2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("NAPIS");
+
+        double a = 0;
+        double b = 1;
+        System.out.println("NAPIS");
+        System.out.println("ZMIENNE");
     }
 }
