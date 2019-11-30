48be61c7239bfbd0be076ca734789f489673d852
Jacek Kołodziejczyk <patagony07@gmail.com>
2019-11-30 13:16:55 +0100

    zmienne3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         double a = 0;
         double b = 1;
+        double c = 2;
+        double d = 3;
         System.out.println("NAPIS");
         System.out.println("ZMIENNE");
     }
